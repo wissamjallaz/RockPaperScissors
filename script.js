04389f04a23e12925bc02c02f66504e9787f78f8
Wissam Allaz <wissam.j.allaz@gmail.com>
2022-05-15 11:43:40 +0300

    Added fn to calc winner

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ function computerPlay(){
    ans = paper;
    else 
    ans = scissors;
+   return ans;
+}
+
+function playRound(playerSelection, computerSelection){
+   if (playerSelection.toUpperCase() == computerSelection.toUpperCase())
+   return decision = 'Tie!!';
+   else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection.toUpperCase()=='PAPER')
+   return decision = 'You Lose! Paper beats Rock';
+   else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection.toUpperCase()=='SCISSORS')
+   return decision = 'You Win! Rock beats Scissors';
+   else if (playerSelection.toUpperCase() == 'PAPER' && computerSelection.toUpperCase()=='ROCK')
+   return decision = 'You Win! Paper beats Rock';
+   else if (playerSelection.toUpperCase() == 'PAPER' && computerSelection.toUpperCase()=='SCISSORS')
+   return decision = 'You Lose! Scissors beats Paper';
+   else if (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection.toUpperCase()=='ROCK')
+   return decision = 'You Lose! Rock beats Scissors';
+   else if (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection.toUpperCase()=='PAPER')
+   return decision = 'You Win! Scissors beats Paper';
 }
 
